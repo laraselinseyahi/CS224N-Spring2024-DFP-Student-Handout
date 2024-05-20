bf2be48b6eccf3524c74e2094c271bc5306dc945
suahmed2003 <suahmed@stanford.edu>
2024-05-20 16:21:57 -0700

    adding the LORA extension, very rough version

---
+++ lora.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class LORAExtension(nn.Module):
+    def __init__(self, model, rank, d):
+        super(LORAExtension, self).__init__() # constructor
+
+        self.model = model # setting the model to be the passed in LLM (will be miniBERT in our case)
+        self.A = nn.Parameter(torch.randn(d, rank))
+        self.B = nn.Parameter(torch.randn(rank, d))
+        self.C = nn.Parameter(torch.randn(d, rank))
+        self.D = nn.Parameter(torch.randn(rank, d))
+
+    def adapt_attention(self, Q, K, V):
+        # adding low rank transformations to each of the query, key, and value matrices 
+        Q = Q + torch.matmul(Q, torch.matmul(self.A, self.B))
+        K = K + torch.matmul(K, torch.matmul(self.A, self.B))
+        V = V + torch.matmul(V, torch.matmul(self.A, self.B))
+        return Q, K, V
+    
+    def adapt_ffn(self, W1):
+        # modifies W1, which is the weight layer of the first linear layer in the FFN
+        return W1 + torch.matmul(W1, torch.matmul(self.C, self.D))
+
+    def forward(self, *input):
+        # Modify the model's forward function to use the adapted attention and FFN
+        pass
