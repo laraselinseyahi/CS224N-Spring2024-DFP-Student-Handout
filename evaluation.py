442694204914abc865d942542d6133a73e797dc3
mquirosbch <mquiros@stanford.edu>
2024-06-06 01:34:51 -0700

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -66,7 +66,8 @@ def model_eval_para(dataloader, model, device):
         logits = model.predict_paraphrase(b_ids1, b_mask1, b_ids2, b_mask2)
         #logits = logits.detach().cpu().numpy()
         #preds = logits.sigmoid().round().flatten()
-        preds = logits.sigmoid().round().flatten().cpu().numpy()
+        #preds = logits.sigmoid().round().flatten().cpu().numpy()
+        preds = torch.sigmoid(logits).round().cpu().numpy().flatten()
 
         b_labels = b_labels.flatten()
         y_true.extend(b_labels)
