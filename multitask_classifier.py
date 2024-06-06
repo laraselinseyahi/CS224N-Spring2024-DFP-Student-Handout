c9d58f543ce3f3257ea318c1d7f94aa22a093621
mquirosbch <mquiros@stanford.edu>
2024-06-05 20:03:07 -0700

    Update multitask_classifier.py

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -271,7 +271,7 @@ def train_multitask(args):
 
         # Check parameter updated only Lora, bias and Layernorm
         for name, param in model.named_parameters():
-            if "lora" in name or "bias" in name or "Norm" in name or "norm" in name or "sentiment" in name:
+            if "lora" in name or "bias" in name or "Norm" in name or "norm" in name or "sentiment" in name or "paraphrase" not in name or "similarity" not in name:
                 assert not torch.equal(initial_params[name], param), f"Parameter {name} did not change but it should have."
             else:
                 assert torch.equal(initial_params[name], param), f"Parameter {name} changed but it should not have."
